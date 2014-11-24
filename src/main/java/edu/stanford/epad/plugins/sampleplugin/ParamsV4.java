053043dec7b10885c0e594ae8c73d19d11e08307
Franco Lamping <francolamping@gmail.com>
2014-11-24 20:05:57 -0200

    Update ParamsV4.java

--- src/main/java/edu/stanford/epad/plugins/sampleplugin/ParamsV4.java
+++ src/main/java/edu/stanford/epad/plugins/sampleplugin/ParamsV4.java
@@ -13,8 +13,8 @@ package edu.stanford.epad.plugins.sampleplugin;
 public class ParamsV4 {
         public final static String xsdFilePath = "requiredFiles/AIM_v4.xsd"; 
         public final static String namespace = "gme://caCORE.caCORE/4.4/edu.northwestern.radiology.AIM";
-        public final static String dbUserName = "admin";
-        public final static String dbUserPassword = "admin";
+        public final static String dbUserName = "guest";
+        public final static String dbUserPassword = "guest";
         public final static String serverUrl = "http://epad-dev5.stanford.edu:8899/exist";
         public final static String collection = "aimV4.dbxml";
 }
